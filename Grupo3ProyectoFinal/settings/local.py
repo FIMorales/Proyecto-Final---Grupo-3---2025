581d8e0835b0097f2eeca6ca8d1f5a4d4301091b
fiorellaailenfioravanti-eng <fiorellaailenfioravanti@gmail.com>
2025-12-13 17:35:20 -0300

    Corrección de la ruta de local.py en settings

--- Grupo3ProyectoFinal/settings/local.py
+++ Grupo3ProyectoFinal/settings/local.py
@@ -1,4 +1,6 @@
 from.base import *
+import os
+from pathlib import Path
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -8,12 +10,11 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
-        'NAME': os.path.join(r"C:\Users\franc\Proyecto final informatorio\ComSiete-Grupo3\Grupo3ProyectoFinal", "db.sqlite3"),
-
+        # SOLUCIÓN: Usar BASE_DIR para apuntar al archivo db.sqlite3
+        'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
-
